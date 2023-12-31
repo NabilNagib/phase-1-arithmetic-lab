41fb921996f5ddb49eae30e43059403b69e4b580
NabilNagib <nnaby@gmail.com>
2023-12-31 14:26:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+let num1 = 62;
+let num2 = 1;
+let multiply = num1 * num2;
+
+let random = Math.floor((Math.random() * 6) + 1);
+
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+
+
+let numberArray = [10, 5, 20, 15];
+let max = Math.max(...numberArray);
